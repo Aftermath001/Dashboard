e5acde9ef69ed6d50bea232a8996058861faa5a0
Aftermath001 <alvinasiachi@gmail.com>
2024-08-07 02:27:03 +0300

    Location List

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ContentHeader from './ContentHeader'
 import '../styles/content.css'
 import Card from './Card'
+import LocationList from './LocationList'
 const Content = () => {
   return (
     <div className='content'>
         <ContentHeader/>
         <Card />
+        <LocationList />
 
     </div>
   )
