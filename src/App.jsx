e5332cc378985e8de295307c0d13561ade581ff1
Aftermath001 <alvinasiachi@gmail.com>
2024-08-09 22:13:53 +0300

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import Sidebar from './components/Sidebar'
 import Content from './components/Content'
 import './App.css'
 import Profile from './components/Profile'
+import Entrypage from './components/Entrypage'
 const App = () => {
   return (
     <div className='dashboard'>
       <Sidebar />
       <div className="dashboard--content">
-        <Content />
-        <Profile />
+        <Entrypage />
+        {/* <Content />
+        <Profile /> */}
         
 
       </div>
